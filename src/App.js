bfa48ae3560bf602a4b4ea88e9b3bb4f7e972a1a
vallakatla Sai chandra <cherry@vallakatlas-Air.bbrouter>
2023-05-28 14:38:21 +0530

    first message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import data from './data.json';
+
+
 
 function App() {
+  const [items] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section >
+      <div className='App'>
+        <div className='left-view'>
+          <h3>Your result</h3>
+          <div className='circle'>
+            <h4>76</h4>
+            <p>of 100</p>
+          </div>
+          <h4>Great</h4>
+          <p>You scored higher than 65% of the people who have taken these tests</p>
+        </div>
+        <div className='right-view'>
+          <h3>Summary</h3>
+          <div className='topics'>
+            {items.map((item, index) => (
+              <div className='badge' key={index} style={{ backgroundColor: `${item.color}25`, color: `${item.color}` }}>
+
+                <h4>
+                  <img src={item.icon} alt={item.category} />
+                  {item.category}
+                </h4>
+                <p><span>{item.score}</span> /100</p>
+              </div>
+            ))}
+          </div>
+          <button>Continue</button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
